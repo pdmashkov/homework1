959a6e6db66730647d38ddf456e806fd59dadcf5
pdmashkov <pdmashkov@rolftech.ru>
2025-01-11 12:51:06 +0300

    add failed test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,12 +5,23 @@ import org.junit.Test;
 
 public class CashbackHackServiceTest {
     @Test
-    public void firstTest(){
+    public void firstTest() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFailedTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
 
         Assert.assertEquals(expected, actual);
     }
